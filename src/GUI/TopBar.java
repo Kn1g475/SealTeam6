6438032cb138b3ebe4bcb27b17f99b00f4a14f0d
patternr <patternr@miamioh.edu>
2016-03-07 20:30:02 -0500

    Added files via upload

--- src/GUI/TopBar.java
+++ src/GUI/TopBar.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
@@ -16,6 +18,10 @@ import javax.swing.JPanel;
 import Main.Constants;
 import Main.Main.ButtonListener;
 
+import javax.swing.JRadioButton;
+
+import Main.FileState;
+
 /**
  * The top bar of the GUI.
  * 
@@ -24,9 +30,14 @@ import Main.Main.ButtonListener;
  */
 @SuppressWarnings("serial")
 public class TopBar extends JPanel {
-
+	
+	public static FileState state = FileState.COURSE;
+	
 	public JLabel fileLabel, errorLabel;
 	public JButton addButton, submitButton, deleteButton;
+	private JRadioButton scheduleRadioButton;
+	private JRadioButton requirementsRadioButton;
+	private JRadioButton courseRadioButton;
 
 	/**
 	 * Creates a new top bar object, takes in a listener class so that this
@@ -37,12 +48,12 @@ public class TopBar extends JPanel {
 	public TopBar(ButtonListener buttonListener) {
 
 		setLayout(new BoxLayout(this, BoxLayout.LINE_AXIS));
-		setPreferredSize(new Dimension(0, Constants.TOP_ROW_HEIGHT));
+		setPreferredSize(new Dimension(795, 55));
 		setBackground(Constants.MENU_BACKGROUND_COLOR);
 		setBorder(BorderFactory.createMatteBorder(0, 0,
 				Constants.TOP_ROW_BORDER_THICKNESS, 0,
 				Constants.TOP_ROW_BORDER_COLOR));
-		
+
 		// set up Icon and Program Title in top left corner
 		JLabel title = new JLabel();
 		Image img = new ImageIcon(Constants.MIAMI_ICON_NAME).getImage();
@@ -54,7 +65,7 @@ public class TopBar extends JPanel {
 		add(title);
 
 		add(Box.createHorizontalGlue());
-		
+
 		// set up all buttons and labels
 		JPanel buttonsGroup = new JPanel();
 		buttonsGroup.setLayout(new BoxLayout(buttonsGroup, BoxLayout.PAGE_AXIS));
@@ -62,20 +73,46 @@ public class TopBar extends JPanel {
 		JPanel fileButtons = new JPanel(new FlowLayout(FlowLayout.RIGHT));
 		fileLabel = new JLabel();
 		fileLabel.setFont(Constants.SMALL_FONT);
+
 		addButton = new JButton("Add File");
 		addButton.setActionCommand("addButton");
 		addButton.addActionListener(buttonListener);
+
 		deleteButton = new JButton("Remove");
 		deleteButton.setActionCommand("deleteButton");
 		deleteButton.setEnabled(false);
 		deleteButton.addActionListener(buttonListener);
+
 		submitButton = new JButton("Submit");
 		submitButton.setActionCommand("submitButton");
 		submitButton.setEnabled(false);
 		submitButton.addActionListener(buttonListener);
 
+		// Radio button =========================================
+		courseRadioButton = new JRadioButton("Course");
+		courseRadioButton.setActionCommand("courseRadioButton");
+		courseRadioButton.setSelected(true);
+		courseRadioButton.addActionListener(new RadioEvent());
+		fileButtons.add(courseRadioButton);
+
+		requirementsRadioButton = new JRadioButton("Requirements");
+		requirementsRadioButton.setActionCommand("requirementsRadioButton");
+		requirementsRadioButton.setSelected(false);
+		requirementsRadioButton.addActionListener(new RadioEvent());
+		fileButtons.add(requirementsRadioButton);
+		
+		scheduleRadioButton = new JRadioButton("Schedule");
+		scheduleRadioButton.setActionCommand("scheduleRadioButton");
+		scheduleRadioButton.setSelected(false);
+		scheduleRadioButton.addActionListener(new RadioEvent());
+		fileButtons.add(scheduleRadioButton);
+		//===========================================================
+		
+		
 		// add buttons and labels to GUI
 		fileButtons.add(fileLabel);
+
+
 		fileButtons.add(addButton);
 		fileButtons.add(deleteButton);
 		fileButtons.add(submitButton);
@@ -88,7 +125,35 @@ public class TopBar extends JPanel {
 
 		buttonsGroup.add(fileButtons);
 		buttonsGroup.add(errorLabel);
-		
+
 		add(buttonsGroup);
+	}
+	// Add functionality to the radio buttons =============================
+	private class RadioEvent implements ActionListener {
+
+		public void actionPerformed(ActionEvent e) {
+			
+			if(e.getActionCommand().equalsIgnoreCase("courseRadioButton")){
+				state = FileState.COURSE;
+				courseRadioButton.setSelected(true);
+				requirementsRadioButton.setSelected(false);
+				scheduleRadioButton.setSelected(false);
+			}
+			
+			if(e.getActionCommand().equalsIgnoreCase("requirementsRadioButton")){
+				state = FileState.REQUIREMENT;
+				requirementsRadioButton.setSelected(true);
+				courseRadioButton.setSelected(false);
+				scheduleRadioButton.setSelected(false);
+				
+			}
+			if(e.getActionCommand().equalsIgnoreCase("scheduleRadioButton")){
+				state = FileState.SCHEDULE;
+				scheduleRadioButton.setSelected(true);
+				courseRadioButton.setSelected(false);
+				requirementsRadioButton.setSelected(false);
+			}
+		}
+		
 	}
 }
