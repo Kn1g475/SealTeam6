0476ddd3a5fb7b4fea7dfc0124f93287e568e6da
patternr <patternr@miamioh.edu>
2016-03-13 19:33:25 -0400

    Added files via upload

--- src/GUI/Report.java
+++ src/GUI/Report.java
@@ -40,7 +40,7 @@ public class Report extends JPanel {
 	 */
 	public Report(Data dataObject) {
 		setBackground(Constants.CONTENT_BACKGROUND_COLOR);
-		if (dataObject.classList.isEmpty()) {
+		if (dataObject.currentList.isEmpty()) {
 			this.add(new JLabel("No data to report on"));
 			return;
 		}
